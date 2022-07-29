8bd5a2738ad8ea978561adfe1c04e6aed575944c
alielrayes <61753691+alielrayes@users.noreply.github.com>
2022-07-29 04:52:19 +0200

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: MediaQuery.of(context).size.width > 600
+            ? Text("web screen")
+            : Text("mobile screen"),
+      ),
+    );
+  }
+}
